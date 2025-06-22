c4dba41ca3cb61f9f3d073d3b6216603ff8d71b2
Yong Kang <chiayongkang@hotmail.com>
2025-06-22 22:49:05 +0800

    Enhance site layout and styling with theme toggle functionality
    - Introduced a new header layout with a responsive navbar and theme switcher for improved user experience.
    - Updated `researcher.scss` to implement CSS variables for theming, enhancing design consistency across light and dark modes.
    - Refactored HTML files to integrate the new header and main content structure, ensuring better organization and readability.
    - Added JavaScript functionality for theme toggling, allowing users to switch between light and dark themes seamlessly.
    - Updated external resources and stylesheets for better performance and visual appeal.

---
+++ static/js/theme-toggle.js
@@ -0,0 +1,39 @@
+(function () {
+  var themeToggle = document.querySelector("#theme-toggle");
+  if (!themeToggle) {
+    return;
+  }
+
+  var currentTheme = localStorage.getItem("theme")
+    ? localStorage.getItem("theme")
+    : null;
+
+  if (currentTheme) {
+    document.documentElement.setAttribute("data-theme", currentTheme);
+
+    if (currentTheme === "dark") {
+      themeToggle.checked = true;
+    }
+  } else {
+    // Check for system preference
+    if (
+      window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches
+    ) {
+      document.documentElement.setAttribute("data-theme", "dark");
+      themeToggle.checked = true;
+    }
+  }
+
+  function switchTheme(e) {
+    if (e.target.checked) {
+      document.documentElement.setAttribute("data-theme", "dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      document.documentElement.setAttribute("data-theme", "light");
+      localStorage.setItem("theme", "light");
+    }
+  }
+
+  themeToggle.addEventListener("change", switchTheme, false);
+})();
